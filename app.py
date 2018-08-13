b4ab1fbcd6590e9956cab12b84494040c41eb8cf
Girish Gopaul <g.gopaul@outlook.com>
2018-08-14 01:56:16 +0400

    Added filename on title-bar + some refactoring

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, send_from_directory
 from flask.json import dumps as json_dumps
 from pygments import highlight
-from pygments.lexers import PythonLexer, get_lexer_by_name, get_lexer_for_filename
+from pygments.lexers import get_lexer_by_name, get_lexer_for_filename
 from pygments.formatters import HtmlFormatter
 from sass import compile as sass_compile
 from uuid import uuid4
@@ -19,7 +19,7 @@ def highlight_code():
     code = json.get('code')
     language = json.get('language')
     filename = json.get('filename')
-    theme = json.get('theme')
+    theme = json.get('theme', 'monokai')
 
     sass_variables = '$main-size: %spx\n' % size
     sass_variables += '$body-color: %s\n' % background
@@ -27,10 +27,10 @@ def highlight_code():
     sass_file = sass_variables + open('assets/styles/main.sass', 'r').read()
     css_output = sass_compile(string=sass_file, indented=True)
 
-    formatter = HtmlFormatter(style='monokai')
+    formatter = HtmlFormatter(style=theme)
     style = css_output + formatter.get_style_defs()
 
-    lexer = get_lexer_for_filename(filename)
+    lexer = get_lexer_by_name(language)
     highlighted = highlight(code, lexer, formatter)
 
     if not os.path.exists('/tmp/highlighter'):
@@ -39,16 +39,20 @@ def highlight_code():
     os.makedirs('/tmp/highlighter/%s' % uuid)
 
     out_file = open('/tmp/highlighter/%s/index.html' % uuid, 'w')
-    out_file.write(render_template('index.jinja2', language=language, highlighted=highlighted, style=style))
+    out_file.write(render_template('index.jinja2', language=language, filename=filename, highlighted=highlighted, style=style))
     out_file.flush()
     os.fsync(out_file)
 
     subprocess.call(['wkhtmltoimage', '/tmp/highlighter/%s/index.html' % uuid, '/tmp/highlighter/%s/index.png' % uuid])
 
-    resp = {"url": "http://localhost:5000/image/%s" % uuid}
+    resp = {"image_url": "http://localhost:5000/image/%s" % uuid, "page_url": "http://localhost:5000/page/%s" % uuid}
 
     return json_dumps(resp)
 
 @app.route('/image/<string:uuid>')
 def image(uuid):
     return send_from_directory('/tmp/highlighter/%s' % uuid, 'index.png', as_attachment=not not request.args.get('download'))
+
+@app.route('/page/<string:uuid>')
+def page(uuid):
+    return send_from_directory('/tmp/highlighter/%s' % uuid, 'index.html', as_attachment=not not request.args.get('download'))
