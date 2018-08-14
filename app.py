71f9653ea2d6647c4ad520f289599bc3c1d526c2
Girish Gopaul <ggopaul0510@gmail.com>
2018-08-14 10:59:01 +0400

    Changed to redis :D

--- app.py
+++ app.py
@@ -11,70 +11,103 @@ import redis
 
 app = Flask(__name__)
 
-def set_defaults(attributes):
-    pass
+
+DEFAULTS = {
+    b'size': b'12',
+    b'background': b'rgb(255, 255, 255)',
+    b'color': b'rgb(50, 100, 120)',
+    b'theme': b'monokai'
+}
+
+
+def redis_conn():
+    return redis.StrictRedis()
+
+
+def get_attributes_from_redis(uuid):
+    return redis_conn().hgetall(uuid)
+
 
 def render(attributes):
-    pass
+    sass_variables = '$main-size: %spx\n' % attributes.get(b'size', DEFAULTS[b'size']).decode('utf-8')
+    sass_variables += '$body-color: %s\n' % attributes.get(b'background', DEFAULTS[b'background']).decode('utf-8')
+    sass_variables += '$back-color: %s\n' % attributes.get(b'color', DEFAULTS[b'color']).decode('utf-8')
+    sass_file = sass_variables + open('assets/styles/main.sass', 'r').read()
+    css_output = sass_compile(string=sass_file, indented=True)
+
+    formatter = HtmlFormatter(style=attributes.get(b'theme', DEFAULTS[b'theme']).decode('utf-8'))
+    style = css_output + formatter.get_style_defs()
+
+    language = attributes.get(b'language').decode('utf-8')
+    lexer = get_lexer_by_name(language)
+    highlighted = highlight(attributes.get(b'code').decode('utf-8'), lexer, formatter)
+
+    return render_template(
+        'index.jinja2',
+        language=language,
+        filename=attributes.get(b'filename').decode('utf-8'),
+        highlighted=highlighted,
+        style=style
+    )
+
 
 @app.route('/highlight', methods=['POST'])
 def highlight_code():
     json = request.get_json()
-    size = json.get('size', '12')
-    background = json.get('background', 'rgb(255, 255, 255)')
-    color = json.get('color', 'rgb(30, 30, 80)')
-    code = json.get('code')
     language = json.get('language')
     filename = json.get('filename')
-    theme = json.get('theme', 'monokai')
+    theme = json.get('theme')
+    size = json.get('size')
+    background = json.get('background')
+    color = json.get('color')
 
-    redis_instance = redis.StrictRedis()
+    redis_instance = redis_conn()
 
     attributes = {
-        'code': code
+        'code': json.get('code')
     }
-    size and attributes['size'] = size
-    background and attributes['background'] = background
-    color and attributes['color'] = color
-    language and attributes['language'] = language
-    filename and attributes['filename'] = filename
-    theme and attributes['theme'] = theme
+    if size:
+        attributes['size'] = size
+    if background:
+        attributes['background'] = background
+    if color:
+        attributes['color'] = color
+    if language:
+        attributes['language'] = language
+    if filename:
+        attributes['filename'] = filename
+    if theme:
+        attributes['theme'] = theme
 
     uuid = uuid4()
     redis_instance.hmset(uuid, attributes)
 
-    sass_variables = '$main-size: %spx\n' % size
-    sass_variables += '$body-color: %s\n' % background
-    sass_variables += '$back-color: %s\n' % color
-    sass_file = sass_variables + open('assets/styles/main.sass', 'r').read()
-    css_output = sass_compile(string=sass_file, indented=True)
+    return json_dumps({
+        "image_url": "http://localhost:5000/image/%s" % uuid,
+        "page_url": "http://localhost:5000/page/%s" % uuid
+    })
 
-    formatter = HtmlFormatter(style=theme)
-    style = css_output + formatter.get_style_defs()
-
-    lexer = get_lexer_by_name(language)
-    highlighted = highlight(code, lexer, formatter)
-
-    if not os.path.exists('/tmp/highlighter'):
-        os.makedirs('/tmp/highlighter')
-    uuid = uuid4()
-    os.makedirs('/tmp/highlighter/%s' % uuid)
-
-    out_file = open('/tmp/highlighter/%s/index.html' % uuid, 'w')
-    out_file.write(render_template('index.jinja2', language=language, filename=filename, highlighted=highlighted, style=style))
-    out_file.flush()
-    os.fsync(out_file)
-
-    subprocess.call(['wkhtmltoimage', '/tmp/highlighter/%s/index.html' % uuid, '/tmp/highlighter/%s/index.png' % uuid])
-
-    resp = {"image_url": "http://localhost:5000/image/%s" % uuid, "page_url": "http://localhost:5000/page/%s" % uuid}
-
-    return json_dumps(resp)
 
 @app.route('/image/<string:uuid>')
 def image(uuid):
-    return send_from_directory('/tmp/highlighter/%s' % uuid, 'index.png', as_attachment=not not request.args.get('download'))
+    if not os.path.exists('/tmp/highlighter'):
+        os.makedirs('/tmp/highlighter')
+
+    out_file = open('/tmp/highlighter/%s.html' % uuid, 'w')
+    out_file.write(render(get_attributes_from_redis(uuid)))
+    out_file.flush()
+    os.fsync(out_file)
+
+    subprocess.call(['wkhtmltoimage', '/tmp/highlighter/%s.html' % uuid, '/tmp/highlighter/%s.png' % uuid])
+
+    resp = send_from_directory('/tmp/highlighter', '%s.png' % uuid, as_attachment=not not request.args.get('download'))
+
+    os.remove('/tmp/highlighter/%s.html' % uuid)
+    os.remove('/tmp/highlighter/%s.png' % uuid)
+
+    return resp
+
 
 @app.route('/page/<string:uuid>')
 def page(uuid):
-    return send_from_directory('/tmp/highlighter/%s' % uuid, 'index.html', as_attachment=not not request.args.get('download'))
+    return render(get_attributes_from_redis(uuid))
